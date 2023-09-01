2f421d3275dc60a74e7cdefb4c0889c7f97b3454
MasonPartridge <masonepartridge@gmail.com>
2023-09-01 16:43:08 -0700

    Feature: Add basic auto scroll to skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDraggable } from "react-use-draggable-scroll";
+import { useStopwatch, useTimer } from "react-timer-hook";
+import ScrollCarousel from "scroll-carousel-react";
 
-import CSSLogo from "@picture/skill_logos/CSS.jpeg";
+import CSSLogo from "@picture/skill_logos/CSS.png";
 import HTMLLogo from "@picture/skill_logos/HTML.png";
 import JSLogo from "@picture/skill_logos/JS.webp";
 import ReactLogo from "@picture/skill_logos/React.png";
@@ -51,49 +53,56 @@ export default function Skills() {
     },
   ];
 
+  const autoScrollInvervalInSeconds = 2;
+  const autoScrollJumpPx = 256;
   const scrollRef = React.useRef(null);
-  const { events } = useDraggable(scrollRef)
+  const { events } = useDraggable(scrollRef);
+  const { seconds, reset } = useStopwatch({ autoStart: true });
 
-  const [isScrolling, setIsScrolling] = useState(false);
-  const [scrollX, setScrollX] = useState(0);
-  const [clientX, setClientX] = useState(0);
+  const [scrollX, setScrollX] = useState(2427);
+  const [isAutoScrolling, setIsAutoScrolling] = useState(true);
 
-  function onMouseDown(event) {
-    setIsScrolling(true);
-    setClientX(event.clientX);
-  }
-
-  function onMouseUp(event) {
-    setIsScrolling(false);
-  }
-
-  function onMouseMove(event) {
-    if (isScrolling) {
-      scrollRef.current.scrollLeft = scrollX + event.clientX - clientX;
-      setScrollX(scrollX + event.clientX - clientX);
-      setClientX(event.clientX);
+  useEffect(() => {
+    console.log(seconds);
+    if (isAutoScrolling && seconds >= autoScrollInvervalInSeconds) {
+      scrollRef.current.scrollLeft =
+        scrollRef.current.scrollLeft < scrollRef.current.scrollWidth
+          ? scrollRef.current.scrollLeft + autoScrollJumpPx
+          : 0;
+      reset();
     }
-  }
+  }, [seconds]);
 
   return (
     <div className="flex flex-col gap-8 justify-center w-[100%] items-center h-[45vh] bg-slate-900 text-white px-32">
       <h1 className="text-[300%] font-bold">Skills</h1>
-      <div
-        {...events}
-        ref={scrollRef}
-        className="flex flex-row gap-8 overflow-x-scroll scroll-hidden w-[100%]"
-      >
-        {skills.map((skill) => {
-          return (
-            <a key={skill.link} className="h-[256px] flex-shrink-0 w-[256px]">
-              <img
-                className="bg-white w-[100%] h-[100%] rounded-full"
-                src={skill.image}
-                alt="logo"
-              />
-            </a>
-          );
-        })}
+      <div className="w-[100%] gap-8 flex">
+        <button className="text-[500%]">&lt;</button>
+        <div
+          {...events}
+          ref={scrollRef}
+          className="flex flex-row gap-8 justify-center overflow-x-scroll cursor-grab scroll-hidden w-[100%]"
+        >
+          {[0, 1].map((loop) => {
+            return skills.map((skill) => {
+              return (
+                <a
+                  target="_blank"
+                  key={skill.link}
+                  href={skill.link}
+                  className="h-[256px] flex-shrink-0 w-[256px]"
+                >
+                  <img
+                    className="bg-white w-[100%] h-[100%] rounded-full object-contain"
+                    src={skill.image}
+                    alt="logo"
+                  />
+                </a>
+              );
+            });
+          })}
+        </div>
+        <button className="text-[500%]">&gt;</button>
       </div>
     </div>
   );
