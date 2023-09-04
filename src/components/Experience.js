e13544bec0abb609e6f848749249f016f00a8533
MasonPartridge <masonepartridge@gmail.com>
2023-09-04 10:12:38 -0700

    Feature: Add personal projects section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,29 @@
 import React from "react";
 
 import CommunityAliThumbnail from "@picture/CommunityAliThumbnail.png";
+import ProfilioThumbnail from "@picture/ProfilioThumbnail.png";
 
 function ExperienceCard(props) {
+  if (props.imageSide === "left") {
+    return (
+      <div className="flex gap-16 bg-slate-900 p-6 rounded-3xl">
+        <img className="w-[600px] h-[314px]" src={props.image} alt="" />
+        <div>
+          <h1 className="text-[300%]">{props.title}</h1>
+          <p>{props.body}</p>
+          <button >Learn More</button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex gap-16 bg-slate-900 p-6 rounded-3xl">
-      <img className="w-[600px]" src={props.image} alt="" />
       <div>
-        <h1 className="text-[32px]">{props.title}</h1>
+        <h1 className="text-[300%]">{props.title}</h1>
         <p>{props.body}</p>
         <button>Learn More</button>
       </div>
+      <img className="w-[600px] h-[314px]" src={props.image} alt="" />
     </div>
   );
 }
@@ -36,16 +49,44 @@ export default function Experience() {
       
       I worked on this project from 2023-02-01 to 2023-09-01.`,
       image: CommunityAliThumbnail,
+      imageSide: "left",
     },
   ];
-  const personalExperience = [];
+  const personalExperience = [
+    {
+      title: "Profilio Website",
+      body: `I worked on this website as a way to showcase my skills and experience
+    as a web developer. I used React.js, tailwindcss, and Webpack for frontend 
+    and am using heroku for hosting. The most challenging part of this project
+    was the carocell component, which is a component that allows you to scroll
+    through all the technologies I have experience with.`,
+      image: ProfilioThumbnail,
+    },
+  ];
 
   return (
-    <div className="px-16 py-32 gap-16 text-white h-[75vh] flex flex-col 
-        items-center bg-slate-800">
-      <h1 className="text-center text-[300%] font-bold">Professional Experience</h1>
+    <div
+      className="px-16 py-32 gap-16 text-white flex flex-col 
+        items-center bg-slate-800"
+    >
+      <h1 className="text-center text-[300%] font-bold">
+        Professional Experience
+      </h1>
       <div>
         {professionalExperience.map((experience) => {
+          return (
+            <ExperienceCard
+              image={experience.image}
+              title={experience.title}
+              body={experience.body}
+              imageSide={experience.imageSide}
+            />
+          );
+        })}
+      </div>
+      <h1 className="text-center text-[300%] font-bold">Personal Experience</h1>
+      <div>
+        {personalExperience.map((experience) => {
           return (
             <ExperienceCard
               image={experience.image}
