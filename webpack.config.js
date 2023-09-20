dccad589422e335d960377e3340cf20aec21e225
MasonPartridge <masonepartridge@gmail.com>
2023-09-20 15:43:05 -0700

    Feature: Add CNAME webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CnameWebpackPlugin = require('cname-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -24,6 +25,9 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
+    }),
+    new CnameWebpackPlugin({
+      domain: 'www.masonpartridge.com',
     }),
 
     new MiniCssExtractPlugin(),
