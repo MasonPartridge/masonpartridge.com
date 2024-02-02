3e5eb0d40cc9dcc8510b21b863bda95d4730b9a3
MasonPartridge <masonepartridge@gmail.com>
2024-02-01 16:19:17 -0800

    feat(navbar): redesign navbar

--- src/pages/landing-page/index.js
+++ src/pages/landing-page/index.js
@@ -36,22 +36,27 @@ export default function LandingPage() {
 
 function NavigationBar() {
   return (
-    <div className="text-white z-50 fixed bg-primary px-4 w-[100%] justify-center flex gap-4 text-lg">
-      <a className="hover:font-bold" href="#home">
-        Home
-      </a>
-      <a className="hover:font-bold" href="#skills">
-        Skills
-      </a>
-      <a className="hover:font-bold" href="#professional-experience">
-        Professional Experience
-      </a>
-      <a className="hover:font-bold" href="#personal-experience">
-        Personal Experience
-      </a>
-      <a className="hover:font-bold" href="#contact-me">
-        Contact Me
-      </a>
+    <div className="text-white z-50 fixed bg-background px-4 border-b-2 p-4
+     border-b-secondary w-[100%] flex justify-center text-lg">
+      <div className="max-w-screen-xl w-full justify-between flex gap-4 ">
+          <a className="hover:font-bold text-xl" href="#home">
+            MASONPARTRIDGE.COM
+          </a>
+        <div className="flex gap-4">
+          <a className="hover:font-bold" href="#skills">
+            Skills
+          </a>
+          <a className="hover:font-bold" href="#professional-experience">
+            Professional Experience
+          </a>
+          <a className="hover:font-bold" href="#personal-experience">
+            Personal Experience
+          </a>
+          <a className="hover:font-bold" href="#contact-me">
+            Contact Me
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
