02032c082d7f9954a3bebfe51450209e10a6cf38
MasonPartridge <masonepartridge@gmail.com>
2024-02-15 16:05:10 -0800

    feat(app.js):build basic express app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 8080;
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
+  app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`)
+  });
